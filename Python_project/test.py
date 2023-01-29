42635e5656754f040f62817dc033ca503a475535
KANCHI REDDY GARI OBI REDDY <93593081+obi-reddy@users.noreply.github.com>
2023-01-29 22:14:34 +0530

    Add files via upload
    The aim of this project is to simulate the progression of Covid-19 in a chosen set of countries using Pandas
    and NumPy. The Markov Chain rule is implemented on each timeline, and the condensed time series is
    saved in ”.CSV” files. To conclude, the spread of Covid-19 is visualized and saved as a ”.PNG” file. This
    project aims to showcase the benefit of using historical data to study the spread of the disease.

---
+++ Python_project/test.py
@@ -0,0 +1,14 @@
+import unittest
+import project
+
+SAMPLE_RATIO = 1e6
+
+# START_DATE = '2020-04-01'
+START_DATE = '2021-04-01'
+END_DATE = '2022-04-30'
+
+class A3Test(unittest.TestCase):
+    def runTest(self):
+        project.run(countries_csv_name='a3-countries.csv', countries=['Afghanistan','Sweden','Japan'], sample_ratio= SAMPLE_RATIO, start_date= START_DATE, end_date= END_DATE)
+
+unittest.main()
